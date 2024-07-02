d966f50aa27cb501c9d13264c8343e39887d9127
ValeriaKornienko <valebog94@gmail.com>
2024-07-02 16:54:30 +0200

    Homework04

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Homework04 from "./homeworks/Homework04/Homework04.jsx";
 import Lesson04 from "./lessons/Lesson04/Lesson04.jsx";
 
 function App() {
   return (
     <div className="App">
-      <Lesson04 />
+      <Homework04 />
     </div>
   );
 }
---
+++ src/homeworks/Homework04/Homework04.jsx
@@ -0,0 +1,8 @@
+import "./styles.css";
+import Feedback from "../../companents/Feedback/Feedback";
+
+function Homework04 () {
+    return <div className="page-wrapper"><Feedback /></div>
+}
+
+export default Homework04;
